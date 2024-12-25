c64c76e8f64bc7b4da4827145975b69d0f4863c2
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-12-25 17:25:03 +0100

    protocollo creazione address bitcoin

--- blockStaking/AddressBlockStaking.js
+++ blockStaking/AddressBlockStaking.js
@@ -1,4 +1,5 @@
 const crypto = require("crypto");
+const fs = require("fs").promises;
 
 function Entopy() {
   const entropy = crypto.randomBytes(16).toString("hex");
@@ -10,7 +11,24 @@ function Entopy() {
   return entropy_bits;
 }
 
-const bits_entropy = Entopy();
-const checksum = crypto.createHash("sha256").update(bits_entropy).digest("hex").slice(0, 1);
-const bits_checksum = bits_entropy + parseInt(checksum, 16).toString(2).padStart(4, "0");
-console.log(bits_checksum);
+async function words(bits) {
+  let list_words = [];
+  const data = await fs.readFile("words.txt", "utf-8");
+  const element = data.toString().split("\n");
+  for (let i = 0; i < bits.length; i += 11) {
+    const chunk = bits.slice(i, i + 11);
+    const number = parseInt(chunk, 2).toString(10);
+    list_words.push(element[number]);
+  }
+
+  return list_words;
+}
+
+async function main() {
+  const bits_entropy = Entopy();
+  const checksum = crypto.createHash("sha256").update(bits_entropy).digest("hex").slice(0, 1);
+  const bits_checksum = bits_entropy + parseInt(checksum, 16).toString(2).padStart(4, "0");
+  console.log(await words(bits_checksum));
+}
+
+main();
