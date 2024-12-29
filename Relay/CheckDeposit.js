ab35ceed4f590a1ea0e69c76580094d15f497fcb
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-12-29 14:41:08 +0100

    usato versione 14 per bitcoin-core essendo che gli piace fare i pompini

--- Relay/CheckDeposit.js
+++ Relay/CheckDeposit.js
@@ -1,3 +1,33 @@
+const Client = require("bitcoin-core");
+// const client = new Client({ network: "regtest" });
+
+// const Peer = new bitcoinCore({ network: "testnet" });
+
+const addressUsers = "mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN";
+const transectionRaw =
+  "02000000014cbcbbe7f06a2d51ace150946691ea4239ee067dc3b81f1a956d183a2723fe6c000000006a47304402206860351004fcbd685d43693be7b05d4c9b57e1a2ce9e7eaa488fe7e7e5460f6d022060c16e210f4188095abb03aa2ea0eb8a6946ba3735adabee096f8fda2bea7c9b012103ce657273af7b6fc1047fb56436961ab9ed57cacc382eeddf47cb63e0bcef760effffffff02e8030000000000001976a91406c0aa6ab779c914b9558cf4a65087fcecff709388ac10ef4b00000000001976a914d320c24246a9245453aa45238e9456fc8aafbcf588ac00000000";
+
+/*
+
+
+testnet-seed.bitcoin.petertodd.org
+testnet-seed.bluematt.me
+testnet-seed.bitcoin.schildbach.de
+
+https://bitnodes.io/
+
+
+https://madpackets.com/2017/12/12/bitcoin-networking/#:~:text=The%20node%2Fservice%20column%20shows%20the%20IPv4%20or%20IPv6,above%2C%20this%20example%20is%20on%20the%20Bitcoin%20testnet.
+
+
+per vedere nodi testnet attivi nslookup testnet-seed.bitcoin.petertodd.org
+ dig testnet-seed.bitcoin.petertodd.org
+
+https://riptutorial.com/bitcoin/example/26240/request-a-merkle-block-with-bitcore-p2p per merkle roort
+
+
+
+
 const bitcoin = require("bitcore-lib");
 const bitcoreP2p = require("bitcore-p2p");
 
@@ -50,21 +80,4 @@ peer.on("disconnect", function () {
 
 // Gestione di altri eventi se necessario
 peer.connect();
-
-const addressUsers = "mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN";
-const transectionRaw =
-  "02000000014cbcbbe7f06a2d51ace150946691ea4239ee067dc3b81f1a956d183a2723fe6c000000006a47304402206860351004fcbd685d43693be7b05d4c9b57e1a2ce9e7eaa488fe7e7e5460f6d022060c16e210f4188095abb03aa2ea0eb8a6946ba3735adabee096f8fda2bea7c9b012103ce657273af7b6fc1047fb56436961ab9ed57cacc382eeddf47cb63e0bcef760effffffff02e8030000000000001976a91406c0aa6ab779c914b9558cf4a65087fcecff709388ac10ef4b00000000001976a914d320c24246a9245453aa45238e9456fc8aafbcf588ac00000000";
-
-/*
-
-
-testnet-seed.bitcoin.petertodd.org
-testnet-seed.bluematt.me
-testnet-seed.bitcoin.schildbach.de
-
-https://bitnodes.io/
-
-per vedere nodi testnet attivi nslookup testnet-seed.bitcoin.petertodd.org
- dig testnet-seed.bitcoin.petertodd.org
-https://riptutorial.com/bitcoin/example/26240/request-a-merkle-block-with-bitcore-p2p per merkle roort
   */
