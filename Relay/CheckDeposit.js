39d238a65c23a971376a954cb7bf15ac38ea490e
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-12-30 11:19:09 +0100

    creazione firma

--- Relay/CheckDeposit.js
+++ Relay/CheckDeposit.js
@@ -1,9 +1,14 @@
 const axios = require("axios");
+const crypto = require("crypto");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
 const addressUsers = "mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN";
 const transectionRaw =
   "02000000014cbcbbe7f06a2d51ace150946691ea4239ee067dc3b81f1a956d183a2723fe6c000000006a47304402206860351004fcbd685d43693be7b05d4c9b57e1a2ce9e7eaa488fe7e7e5460f6d022060c16e210f4188095abb03aa2ea0eb8a6946ba3735adabee096f8fda2bea7c9b012103ce657273af7b6fc1047fb56436961ab9ed57cacc382eeddf47cb63e0bcef760effffffff02e8030000000000001976a91406c0aa6ab779c914b9558cf4a65087fcecff709388ac10ef4b00000000001976a914d320c24246a9245453aa45238e9456fc8aafbcf588ac00000000";
 const amount = 500000; // in satoshi
 
+// transazione dell address
 async function transectionAddress(address) {
   // const data = await axios.get("https://blockstream.info/api/address/${address}/txs", {
   // timeout: 10000,
@@ -15,24 +20,50 @@ async function transectionAddress(address) {
   return transactions.data;
 }
 
+// validazione
 async function validate(txs) {
   const heightTip = await axios.get("https://mempool.space/testnet4/api/blocks/tip/height");
 
   txs.forEach((tx) => {
     const checkHeight = heightTip.data - tx.status.block_height;
-    if (5 < checkHeight && checkHeight < 103) {
+    if (5 < checkHeight) {
+      // && checkHeight < 103
       console.log("controllo");
       validateOutput(tx, amount, addressUsers);
     }
   });
 }
 
+// validazione output
 function validateOutput(tx, satoshi, address) {
+  // console.log(tx.status.block_time, tx.txid);
   tx.vout.forEach((txOutput) => {
     if (txOutput.value === satoshi && txOutput.scriptpubkey_address === address) {
+      const para = {
+        txid: tx.txid,
+        amount: satoshi,
+        address: txOutput.scriptpubkey_address,
+        time: tx.status.block_time,
+      };
+      sign(para);
       console.log("passo");
     }
   });
+}
+
+// creazione firma
+function sign(p) {
+  const pairkey = ec.genKeyPair();
+  const publicKey = pairkey.getPublic();
+  var hexMessage = "";
+  for (let key in p) {
+    hexMessage += p[key].toString();
+  }
+  const byte = Buffer.from(hexMessage);
+  const sign = pairkey.sign(byte).toDER("hex");
+
+  // inviare firma e emssaggio a smart contract
+  console.log(pairkey.verify(byte, sign));
 }
 
 async function main() {
@@ -41,6 +72,9 @@ async function main() {
 }
 
 main();
+
+// ora se tutte le verifiche passano bisogna creare una firma e mandarla al debt issuer in modo che capisca che il block
+// è stato avvenuto
 
 /*
 testnet-seed.bitcoin.petertodd.org
--- Relay/README.md
+++ Relay/README.md
@@ -2,3 +2,10 @@ https://api.blockchair.com/bitcoin/nodes
 
 per ora otterrò le transazioni tramite api (ora sto usando la testnet come prova e test) più avanti si proverà a chiedere
 direttamente ai nodi le transazioni degli address come in rem e wormhole
+
+processo di rem per il lock and mint After witnessing the locking of assets, RenVM returns a “minting signature” to the user
+
+RenVM uses the RZL MPC algorithm to produce and return a minting signature to Alice.
+
+
+https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm 
