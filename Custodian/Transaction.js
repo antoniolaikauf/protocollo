24bb688106e52f020489ecbd5b313a3b18ada5ec
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-01-16 22:10:17 +0100

    transazione raw base

--- Custodian/Transaction.js
+++ Custodian/Transaction.js
@@ -5,28 +5,28 @@ const bs58check = require("bs58check");
 const EC = require("elliptic").ec; // dipendenza da installare
 const ec = new EC("secp256k1");
 
-const address_testFrom = "2NFEgHLofKiFz19Sa7eqGAbMkCoa4b1dtcr";
-const address_testTo = "mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN";
+const address_testTo = "2NFEgHLofKiFz19Sa7eqGAbMkCoa4b1dtcr";
+const address_testFrom = "mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN";
 const inputIndex = 1;
-const transaction = "0135207e916986769a092c18c89a10eee1bbc811d4da2353418f640bc373b904";
-const amount = "6a4";
+const transaction = "fe4b60b2da921ff3a1b94ed3bb4765e683ac99db855ef466e6d34d52c77c3f18";
+const amount = "7d0";
 const privateKey = "793e4754ba6305f53afff74100e0d127ff548e1294955c2296811b6ec7c0be1f";
 
 class Transaction {
   constructor(addressTo, addressFrom, inputIndex, transaction, amount) {
-    this.version = reverse("1".padStart(8, "0"));
+    this.lookTime = reverse("00000000");
+    this.hashCodeType = reverse("00000001");
+    this.amountOutput = Buffer.from([0x01]);
+    this.amount = this.valueOutput(amount).padEnd(16, "0");
+    this.sequenza = Buffer.from("ffffffff", "hex");
+    this.scriptPubKey = this.reedem(addressTo, "P2SH");
+    this.version = reverse("2".padStart(8, "0"));
     this.amountInput = Buffer.from([0x01]);
     this.transaction = reverse(transaction);
     this.inputIndex = reverse(inputIndex.toString().padStart(8, "0"));
     this.LenghtScriptSig = Buffer.from([0x19]); // 25 bytes
-    this.emptyScript = this.reedem("mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN");
+    this.emptyScript = this.reedem(addressFrom, "P2PKH");
     this.ScriptSig = this.scriptSig();
-    this.sequenza = Buffer.from("ffffffff", "hex");
-    this.amountOutput = Buffer.from([0x01]);
-    this.amount = this.valueOutput(amount).padEnd(16, "0");
-    this.scriptPubKey = this.reedem(addressTo);
-    this.lookTime = reverse("00000000");
-    this.hashCodeType = reverse("00000001");
     this.addressFrom = addressFrom;
     this.addressTo = addressTo;
   }
@@ -41,17 +41,29 @@ class Transaction {
     return Buffer.from(p, "hex").reverse().join("");
   }
 
-  reedem(address) {
+  reedem(address, typeAddress) {
     const decoded = bs58check.default.decode(address);
     const redeemScriptAddress = decoded.slice(1);
-    return this.script(redeemScriptAddress);
+    if (typeAddress == "P2PKH") return this.script(redeemScriptAddress);
+    else if (typeAddress == "P2SH") return this.scriptP2SH(redeemScriptAddress);
+  }
+
+  scriptP2SH(bytes) {
+    const S = Buffer.concat([
+      Buffer.from([0xa9]), // ---------------
+      Buffer.from([0x14]), // butta su 20 bytes
+      Buffer.from(bytes, "hex"),
+      Buffer.from([0x87]),
+    ]);
+
+    return S;
   }
 
   script(bytes) {
     const S = Buffer.concat([
       Buffer.from([0x76]), // ---------------------
       Buffer.from([0xa9]),
-      Buffer.from([0x19]),
+      Buffer.from([0x14]), // butta su 20 bytes
       Buffer.from(bytes, "hex"),
       Buffer.from([0x88]),
       Buffer.from([0xac]),
@@ -63,23 +75,30 @@ class Transaction {
   doubleHash(dataToHash) {
     return crypto.createHash("sha256").update(crypto.createHash("sha256").update(dataToHash).digest()).digest("hex");
   }
+
   // data for sign
   createData() {
+    const tra =
+      "02000000000101d923fcc2b77ce7e286d232885cc224f1f39b6a26cf1742877b38ddb4aaa6dec30000000000fdffffff0347a616c3000000001976a9143b4e6e057ca085de765a1deb7dbf92437ba216b788aca0860100000000001976a914d320c24246a9245453aa45238e9456fc8aafbcf588ac0000000000000000196a176661756365742e746573746e6574342e6465762074786e014057b68abd6c2ff5acd88cc30f4b191e38ba900533def654fdf16288ff88e330bc1c24b4a381e563411bcd76dff7f2e22c5706cdb6d0b84da1c338654445ad3e9e00000000";
+    const r = doubleHash(Buffer.from(tra), "hex");
     const data = Buffer.concat([
       Buffer.from(this.version, "hex"),
       this.amountInput, //----------------------
-      Buffer.from(this.transaction, "hex"),
+      Buffer.from(r, "hex"),
       Buffer.from(this.inputIndex, "hex"),
       Buffer.from([0x19]),
       this.emptyScript,
       this.sequenza,
       this.amountOutput,
       Buffer.from(this.amount, "hex"),
-      Buffer.from(this.scriptPubKey.length.toString()),
+      Buffer.from(this.scriptPubKey.length.toString(16)),
       this.scriptPubKey,
       Buffer.from(this.lookTime, "hex"),
       Buffer.from(this.hashCodeType, "hex"),
     ]);
+
+    console.log(tra);
+
     return data;
   }
 
@@ -95,12 +114,12 @@ class Transaction {
       this.amountInput, //----------------------
       Buffer.from(this.transaction, "hex"),
       Buffer.from(this.inputIndex, "hex"),
-      Buffer.from(this.ScriptSig.length.toString()),
+      Buffer.from(this.ScriptSig.length.toString(16), "hex"),
       this.ScriptSig,
       this.sequenza,
       this.amountOutput,
       Buffer.from(this.amount, "hex"),
-      Buffer.from(this.scriptPubKey.length.toString()),
+      Buffer.from(this.scriptPubKey.length.toString(16), "hex"),
       this.scriptPubKey,
       Buffer.from(this.lookTime, "hex"),
     ]);
@@ -108,13 +127,15 @@ class Transaction {
   }
 
   scriptSig() {
-    const publicKeyNotCompress =
-      "04ce657273af7b6fc1047fb56436961ab9ed57cacc382eeddf47cb63e0bcef760e64c361b1b65b82b2ee9d804f06bb9637e41c785cd7657d85a6259abb1bdc86f7";
+    const publicKeyNotCompress = "03ce657273af7b6fc1047fb56436961ab9ed57cacc382eeddf47cb63e0bcef760e";
+    // const publicKeyNotCompress =
+    // "04ce657273af7b6fc1047fb56436961ab9ed57cacc382eeddf47cb63e0bcef760e64c361b1b65b82b2ee9d804f06bb9637e41c785cd7657d85a6259abb1bdc86f7";
     const sign = Buffer.from(this.sign(privateKey), "hex");
+
     const script = Buffer.concat([
-      Buffer.from(sign.length.toString()), // ----------------
+      Buffer.from(sign.length.toString(16), "hex"), // ----------------
       sign,
-      Buffer.from(publicKeyNotCompress.length.toString()),
+      Buffer.from(publicKeyNotCompress.length.toString(16), "hex"),
       Buffer.from(publicKeyNotCompress, "hex"),
     ]);
 
@@ -122,10 +143,32 @@ class Transaction {
   }
 }
 
+/*
+sto sbagliando lo scriptsig 
+
+mia      0200000001183f7cc7524dd3e666f45e85db99ac83e66547bbd34eb9a1f31f92dab2604bfe01000000
+6b4830450220032eae2326cbbfc4e98d48f3107a514edab69f7c902bdb2a35b7e92d1d52d0be022100f22ae5740a7c3ba7c81ec3d91e276c99e2ab69c88f477da80db1e321d4ee7ecb0142
+03ce657273af7b6fc1047fb56436961ab9ed57cacc382eeddf47cb63e0bcef760e
+ffffffff
+01
+d007000000000000
+17a914f1384ced7248c3db7fe1950d415772291fafae8487
+00000000
+
+libreria 0200000001183f7cc7524dd3e666f45e85db99ac83e66547bbd34eb9a1f31f92dab2604bfe01000000
+6a473044022062ef09ee63c91d4f4f969c63582649c5d71ac7df702cca6c2c616a68fe7d831802202459fb4e252169749b4041244d530ba79000aa2c3967bf555b71902aed2dbd5b0121
+03ce657273af7b6fc1047fb56436961ab9ed57cacc382eeddf47cb63e0bcef760e
+ffffffff
+01
+d007000000000000
+17a914f1384ced7248c3db7fe1950d415772291fafae8487
+00000000
+
+*/
 const transaction1 = new Transaction(address_testTo, address_testFrom, inputIndex, transaction, amount);
 
 console.log(transaction1);
-console.log(transaction1.createTransactions().toString("hex"));
+console.log("la transazione mia: " + transaction1.createTransactions().toString("hex"));
 
 //---------------------------------------------------
 // controllare sopra
@@ -140,29 +183,30 @@ function createTransection(pk) {
   // const address_test = "2MwVDMAhEX8WptvMNLRrofm4VY6t6K4j1qg"; // address a cui inviare soldi
 
   const utxos = {
-    txId: "0135207e916986769a092c18c89a10eee1bbc811d4da2353418f640bc373b904",
+    txId: "fe4b60b2da921ff3a1b94ed3bb4765e683ac99db855ef466e6d34d52c77c3f18",
     outputIndex: 1,
     address: "mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN",
     script: bitcoin.Script.buildPublicKeyHashOut(addressMoney),
     satoshis: 100000, // satoshi che ha la transazione
   };
 
-  console.log(addressMoney.toString("hex"));
-  console.log("Match:", addressMoney.toString("hex") === utxos.address);
+  // console.log(addressMoney.toString("hex"));
+  // console.log("Match:", addressMoney.toString("hex") === utxos.address);
 
   const fee = 1000;
   var transaction = new bitcoin.Transaction()
     .from(utxos) // Feed information about what unspent outputs one can use
     .to(address_test, 2000) // Add an output with the given amount of satoshis
-    .change("mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN") // Sets up a change address where the rest of the funds will go
-    .fee(fee)
+    // .change("mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN") // Sets up a change address where the rest of the funds will go
+    // .fee(fee)
     .sign(privateKey);
 
+  console.log("la transazione libreria: " + transaction);
   return transaction.serialize();
 }
 
 function broadcast(tx) {
-  console.log(tx);
+  // console.log(tx);
 
   const transactions = axios
     .post("https://mempool.space/testnet4/api/tx", tx, {
@@ -248,10 +292,10 @@ function main() {
   const privateKey = "793e4754ba6305f53afff74100e0d127ff548e1294955c2296811b6ec7c0be1f";
   // const transection = createTransection1();
   const transection = createTransection(privateKey);
-  broadcast(transection);
+  // broadcast(transection);
 }
 
-// main();
+main();
 
 /*
 // https://github.com/bitpay/bitcore-lib/blob/master/docs/examples.md
@@ -389,166 +433,3 @@ function main1() {
 }
 
 main1();
-
-/*
-
-
-#bitcointools
-from deserialize import parse_Transaction, opcodes
-from BCDataStream import BCDataStream
-from base58 import bc_address_to_hash_160, b58decode, public_key_to_bc_address, hash_160_to_bc_address
-
-import ecdsa_ssl
-
-import Crypto.Hash.SHA256 as sha256
-import Crypto.Random
-
-#transaction, from which we want to redeem an output
-HEX_TRANSACTION="010000000126c07ece0bce7cda0ccd14d99e205f118cde27e83dd75da7b141fe487b5528fb000000008b48304502202b7e37831273d74c8b5b1956c23e79acd660635a8d1063d413c50b218eb6bc8a022100a10a3a7b5aaa0f07827207daf81f718f51eeac96695cf1ef9f2020f21a0de02f01410452684bce6797a0a50d028e9632be0c2a7e5031b710972c2a3285520fb29fcd4ecfb5fc2bf86a1e7578e4f8a305eeb341d1c6fc0173e5837e2d3c7b178aade078ffffffff02b06c191e010000001976a9143564a74f9ddb4372301c49154605573d7d1a88fe88ac00e1f505000000001976a914010966776006953d5567439e5e39f86a0d273bee88ac00000000"
-#output to redeem. must exist in HEX_TRANSACTION
-OUTPUT_INDEX=1
-#address we want to send the redeemed coins to.
-#REPLACE WITH YOUR OWN ADDRESS, unless you're feeling generous 
-SEND_TO_ADDRESS="1L4xtXCdJNiYnyqE6UsB8KSJvqEuXjz6aK"
-#fee we want to pay (in BTC)
-TX_FEE=0.001
-#constant that defines the number of Satoshis per BTC
-COIN=100000000
-#constant used to determine which part of the transaction is hashed.
-SIGHASH_ALL=1
-#private key whose public key hashes to the hash contained in scriptPubKey of output number *OUTPUT_INDEX* in the transaction described in HEX_TRANSACTION
-PRIVATE_KEY=0x18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725
-
-def dsha256(data):
-   return sha256.new(sha256.new(data).digest()).digest()
-
-tx_data=HEX_TRANSACTION.decode('hex_codec')
-tx_hash=dsha256(tx_data)
-
-#here we use bitcointools to parse a transaction. this gives easy access to the various fields of the transaction from which we want to redeem an output
-stream = BCDataStream()
-stream.write(tx_data)
-tx_info = parse_Transaction(stream)
-
-if len(tx_info['txOut']) < (OUTPUT_INDEX+1):
-   raise RuntimeError, "there are only %d output(s) in the transaction you're trying to redeem from. you want to redeem output index %d" % (len(tx_info['txOut']), OUTPUT_INDEX)
-
-#this dictionary is used to store the values of the various transaction fields
-#  this is useful because we need to construct one transaction to hash and sign
-#  and another that will be the final transaction
-tx_fields = {}
-
-##here we start creating the transaction that we hash and sign
-sign_tx = BCDataStream()
-##first we write the version number, which is 1
-tx_fields['version'] = 1
-sign_tx.write_int32(tx_fields['version'])
-##then we write the number of transaction inputs, which is one
-tx_fields['num_txin'] = 1
-sign_tx.write_compact_size(tx_fields['num_txin'])
-
-##then we write the actual transaction data
-#'prevout_hash'
-tx_fields['prevout_hash'] = tx_hash
-sign_tx.write(tx_fields['prevout_hash']) #hash of the the transaction from which we want to redeem an output
-#'prevout_n'
-tx_fields['output_index'] = OUTPUT_INDEX
-sign_tx.write_uint32(tx_fields['output_index']) #which output of the transaction with tx id 'prevout_hash' do we want to redeem?
-
-##next comes the part of the transaction input. here we place the script of the *output* that we want to redeem
-tx_fields['scriptSigHash'] = tx_info['txOut'][OUTPUT_INDEX]['scriptPubKey']
-#first write the size
-sign_tx.write_compact_size(len(tx_fields['scriptSigHash']))
-#then the data
-sign_tx.write(tx_fields['scriptSigHash'])
-
-#'sequence'
-tx_fields['sequence'] = 0xffffffff
-sign_tx.write_uint32(tx_fields['sequence'])
-
-##then we write the number of transaction outputs. we'll just use a single output in this example
-tx_fields['num_txout'] = 1
-sign_tx.write_compact_size(tx_fields['num_txout'])
-##then we write the actual transaction output data
-#we'll redeem everything from the original output minus TX_FEE
-tx_fields['value'] = tx_info['txOut'][OUTPUT_INDEX]['value']-(TX_FEE*COIN)
-sign_tx.write_int64(tx_fields['value'])
-##this is where our scriptPubKey goes (a script that pays out to an address)
-#we want the following script:
-#"OP_DUP OP_HASH160  OP_EQUALVERIFY OP_CHECKSIG"
-address_hash = bc_address_to_hash_160(SEND_TO_ADDRESS)
-#chr(20) is the length of the address_hash (20 bytes or 160 bits)
-scriptPubKey = chr(opcodes.OP_DUP) + chr(opcodes.OP_HASH160) + \
-   chr(20) + address_hash + chr(opcodes.OP_EQUALVERIFY) + chr(opcodes.OP_CHECKSIG)
-#first write the length of this lump of data
-tx_fields['scriptPubKey'] = scriptPubKey
-sign_tx.write_compact_size(len(tx_fields['scriptPubKey']))
-#then the data
-sign_tx.write(tx_fields['scriptPubKey'])
-
-#write locktime (0)
-tx_fields['locktime'] = 0
-sign_tx.write_uint32(tx_fields['locktime'])
-#and hash code type (1)
-tx_fields['hash_type'] = SIGHASH_ALL
-sign_tx.write_int32(tx_fields['hash_type'])
-
-#then we obtain the hash of the signature-less transaction (the hash that we sign using our private key)
-hash_scriptless = dsha256(sign_tx.input)
-
-##now we begin with the ECDSA stuff.
-## we create a private key from the provided private key data, and sign hash_scriptless with it
-## we also check that the private key's corresponding public key can actually redeem the specified output
-
-k = ecdsa_ssl.KEY()
-k.generate(('%064x' % PRIVATE_KEY).decode('hex'))
-
-#here we retrieve the public key data generated from the supplied private key
-pubkey_data = k.get_pubkey()
-#then we create a signature over the hash of the signature-less transaction
-sig_data=k.sign(hash_scriptless)
-#a one byte "hash type" is appended to the end of the signature (https://en.bitcoin.it/wiki/OP_CHECKSIG)
-sig_data = sig_data + chr(SIGHASH_ALL)
-
-#let's check that the provided privat key can actually redeem the output in question
-if (bc_address_to_hash_160(public_key_to_bc_address(pubkey_data)) != tx_info['txOut'][OUTPUT_INDEX]['scriptPubKey'][3:-2]):
-   bytes = b58decode(SEND_TO_ADDRESS, 25)
-   raise RuntimeError, "The supplied private key cannot be used to redeem output index %d\nYou need to supply the private key for address %s" % \
-                           (OUTPUT_INDEX, hash_160_to_bc_address(tx_info['txOut'][OUTPUT_INDEX]['scriptPubKey'][3:-2], bytes[0]))
-
-##now we begin creating the final transaction. this is a duplicate of the signature-less transaction,
-## with the scriptSig filled out with a script that pushes the signature plus one-byte hash code type, and public key from above, to the stack
-
-final_tx = BCDataStream()
-final_tx.write_int32(tx_fields['version'])
-final_tx.write_compact_size(tx_fields['num_txin'])
-final_tx.write(tx_fields['prevout_hash'])
-final_tx.write_uint32(tx_fields['output_index'])
-
-##now we need to write the actual scriptSig.
-## this consists of the DER-encoded values r and s from the signature, a one-byte hash code type, and the public key in uncompressed format
-## we also need to prepend the length of these two data pieces (encoded as a single byte
-## containing the length), before each data piece. this length is a script opcode that tells the
-## Bitcoin script interpreter to push the x following bytes onto the stack
-
-scriptSig = chr(len(sig_data)) + sig_data + chr(len(pubkey_data)) + pubkey_data
-#first write the length of this data
-final_tx.write_compact_size(len(scriptSig))
-#then the data
-final_tx.write(scriptSig)
-
-##and then we simply write the same data after the scriptSig that is in the signature-less transaction,
-#  leaving out the four-byte hash code type (as this is encoded in the single byte following the signature data)
-
-final_tx.write_uint32(tx_fields['sequence'])
-final_tx.write_compact_size(tx_fields['num_txout'])
-final_tx.write_int64(tx_fields['value'])
-final_tx.write_compact_size(len(tx_fields['scriptPubKey']))
-final_tx.write(tx_fields['scriptPubKey'])
-final_tx.write_uint32(tx_fields['locktime'])
-
-#prints out the final transaction in hex format (can be used as an argument to bitcoind's sendrawtransaction)
-print final_tx.input.encode('hex')
-
-
-*/
