542e051cf02f6317c8d85e69484a21385e8b5e62
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-01-25 11:04:31 +0100

    delete trash

--- Custodian/Transaction.js
+++ Custodian/Transaction.js
@@ -306,10 +306,11 @@ function createTransection(pk) {
     .from(utxos) // Feed information about what unspent outputs one can use
     .to(address_test, 90000) // Add an output with the given amount of satoshis
     .change("mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN") // Sets up a change address where the rest of the funds will go
-    .fee(fee)
-    .sign(privateKey);
+    .fee(fee);
+    
+    transaction.sign(privateKey);
+    console.log("la transazione libreria: " + JSON.stringify(utxos));
 
-  console.log("la transazione libreria: " + transaction);
   return transaction.serialize();
 }
 
@@ -331,78 +332,10 @@ function broadcast(tx) {
     });
 }
 
-function doubleHash(s) {
-  const first_hash = crypto.createHash("sha256").update(s).digest();
-  const scriptHash = crypto.createHash("ripemd160").update(first_hash).digest();
-  return scriptHash;
-}
-
-function createTransection1() {
-  // bitcoin.Networks.add(bitcoin.Networks.testnet);
-  const privateKeyP2sh = "c1cd06feafdd586592ca213a1b1a6d43fe51e26be37ab536e01f71e072198699";
-  // var privateKey = new bitcoin.PrivateKey(privateKeyP2sh);
-  // const addressMoney = privateKey.toAddress(bitcoin.Networks.testnet);
-  // console.log("ccccccccccccccc    " + addressMoney);
-  const address_test = "mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN"; // address a cui inviare soldi
-  const PK = "0210433f033ccf466055e44306c642016d899b976b58388a4f0ed24b3885dae600";
-  const SK2 = "1edbbd0762c5ecf546714ab9c2b11c3d47a6a6f88ea373807b3a14931456e982";
-  const keyPair = ec.keyFromPrivate(SK2);
-  const PK2 = keyPair.getPublic(true, "hex");
-
-  const redeemScript = Buffer.concat([
-    Buffer.from([0x51]), // OP_1 (
-    Buffer.from([0x21]),
-    Buffer.from(PK, "hex"), // PubKey1 (33 bytes for compressed key)
-    Buffer.from([0x21]),
-    Buffer.from(PK2, "hex"), // PubKey2
-    Buffer.from([0x52]), // OP_2 (2 keys)
-    Buffer.from([0xae]), // OP_CHECKMULTISIG
-  ]);
-
-  const first_hash = crypto.createHash("sha256").update(redeemScript).digest();
-  const scriptHash = crypto.createHash("ripemd160").update(first_hash).digest();
-
-  console.log(scriptHash.toString("hex"));
-
-  const utxos = {
-    txId: "3663b6ab3beb6350ce0ab0485a89df1ca65a96fdded300ffc4afc5afea19bb20",
-    outputIndex: 0,
-    address: "2N2YtB7mj7sYrw8jAtNakhyikCTDu6HRfhc",
-    script: bitcoin.Script.buildScriptHashOut(bitcoin.Script(redeemScript)),
-    satoshis: 50000, // satoshi che ha la transazione
-  };
-
-  const fee = 1000;
-  var transaction = new bitcoin.Transaction()
-    .from(utxos, redeemScript) // Feed information about what unspent outputs one can use
-    .to(address_test, 30000) // Add an output with the given amount of satoshis
-    .change("2N2YtB7mj7sYrw8jAtNakhyikCTDu6HRfhc") // Sets up a change address where the rest of the funds will go
-    .fee(fee);
-
-  // Private key for signing
-  const privateKey = new bitcoin.PrivateKey(privateKeyP2sh);
-  const privateKey2 = new bitcoin.PrivateKey(SK2);
-
-  console.log(transaction);
-
-  // Sign the transaction with the private key
-  transaction.sign(privateKey);
-  transaction.sign(privateKey2);
-
-  // // Add signature to the input (P2SH Multisig)
-  // const scriptSig = bitcoin.Script.buildScriptHashIn(redeemScript, [
-  //   Buffer.concat([signature.toDER(), Buffer.from([bitcoin.Transaction.SIGHASH_ALL])]),
-  // ]);
-  // transaction.inputs[0].setScript(scriptSig);
-
-  // console.log("la transazione libreria:  sono uaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa           \n" + transaction.serialize());
-  return transaction.serialize();
-}
 
 function main() {
   const privateKey = "793e4754ba6305f53afff74100e0d127ff548e1294955c2296811b6ec7c0be1f";
   const transection = createTransection1();
-  // const transection = createTransection(privateKey);
   // broadcast(transection);
 }
 
@@ -416,130 +349,3 @@ main();
 // https://faucet.testnet4.dev/
 
 */
-
-function reverse(tx) {
-  return Buffer.from(tx, "hex").reverse().toString("hex");
-}
-
-function main1() {
-  const privateKey2 = new bitcoin.PrivateKey("191c609103e968dc71954d68c8fbe19840673827c672a81e645987b8b514b9e9", bitcoin.Networks.testnet);
-  const publicKey2 = privateKey2.toPublicKey().toString();
-  const p = doubleHash(publicKey2);
-
-  const destinationAddress = "mzmJ7eqgfrqvYGbuMNQtsyEQHrbbQ6XkwN";
-  const decoded = bs58check.default.decode(destinationAddress);
-  const redeemScriptAddress = decoded.slice(1);
-
-  const address = "2NFEgHLofKiFz19Sa7eqGAbMkCoa4b1dtcr";
-  const decodedP2SH = bs58check.default.decode(address);
-  const p2shHash = Buffer.from(decodedP2SH.slice(1), "hex");
-  const privateKeyAddressTo = bitcoin.PrivateKey("793e4754ba6305f53afff74100e0d127ff548e1294955c2296811b6ec7c0be1f", bitcoin.Networks.testnet);
-
-  const scriptAddressFrom = bitcoin.Script.buildPublicKeyHashOut(address);
-  const publicKeyHash = doubleHash(publicKey2);
-  const scriptAddress = bitcoin.Script.buildPublicKeyHashOut(destinationAddress); // script in address
-  const publicKey = doubleHash(privateKeyAddressTo.toPublicKey().toString("hex")).toString("hex");
-
-  // script address from OP_DUP OP_HASH160 20 0xf1384ced7248c3db7fe1950d415772291fafae84 OP_EQUALVERIFY OP_CHECKSIG
-
-  const t = doubleHash("03f6a19b3cf3240dad60914626e293410008821f63fc05add0aaf5803791c27a33");
-
-  const script = Buffer.concat([
-    Buffer.from([0x76]),
-    Buffer.from([0xa9]),
-    Buffer.from([0x14]),
-    p2shHash, //--------------------s
-    Buffer.from([0x88]),
-    Buffer.from([0xac]),
-  ]);
-
-  // console.log("sono qua \n\n" + doubleHash(script).toString("hex") + "\n\n");
-
-  // script address to OP_DUP OP_HASH160 20 0xd320c24246a9245453aa45238e9456fc8aafbcf5 OP_EQUALVERIFY OP_CHECKSIG
-
-  // corretto
-  const ScriptPubKey = Buffer.concat([
-    Buffer.from([0x76]),
-    Buffer.from([0xa9]),
-    Buffer.from([0x14]),
-    redeemScriptAddress,
-    Buffer.from([0x88]),
-    Buffer.from([0xac]),
-  ]);
-
-  // console.log(ScriptPubKey.toString("hex"));
-
-  const version = reverse("2".padStart(8, "0"));
-  const tx_in_count = "01";
-  // input
-  const hashTXReverse = reverse("2353086dbe5fa4c739408216cc319659df0b3bff41a01456ee702e7e021112e6");
-  const indexPrevOutput = reverse("0".padStart(8, "0"));
-
-  const emptyScript = "76a914" + doubleHash(p.toString("hex")).toString("hex") + "88ac";
-  const emptyScriptLenght = emptyScript.length.toString(16);
-  const sequence = "ffffffff";
-  // output
-  const tx_out_count = "01";
-  // aggiunto 0
-  const valueOutput = reverse("06a4").toString("hex").padEnd(16, "0"); // 1700
-  const scriptLenghtOutput = ScriptPubKey.length.toString(16).padStart(2, "0");
-
-  const lookTime = reverse("00000000");
-  const sigHashCode = reverse("00000001");
-
-  const dataToSign = Buffer.concat([
-    Buffer.from(version, "hex"),
-    Buffer.from(tx_in_count, "hex"),
-    Buffer.from(hashTXReverse, "hex"),
-    Buffer.from(indexPrevOutput, "hex"),
-    Buffer.from(emptyScriptLenght, "hex"),
-    Buffer.from(emptyScript, "hex"),
-    Buffer.from(sequence, "hex"),
-    Buffer.from(tx_out_count, "hex"),
-    Buffer.from(valueOutput, "hex"),
-    Buffer.from(scriptLenghtOutput, "hex"),
-    ScriptPubKey, // È già un buffer, non serve convertirlo
-    Buffer.from(lookTime, "hex"),
-    Buffer.from(sigHashCode, "hex"),
-  ]);
-
-  const data1 = crypto.createHash("sha256").update(dataToSign).digest();
-  const data = crypto.createHash("sha256").update(data1).digest();
-
-  const keyPair = ec.keyFromPrivate("191c609103e968dc71954d68c8fbe19840673827c672a81e645987b8b514b9e9");
-
-  const sign = keyPair.sign(data);
-
-  const signatureWithHashType = Buffer.concat([Buffer.from(sign.toDER("hex"), "hex"), Buffer.from([0x01])]);
-
-  const scriptSig = Buffer.concat([
-    Buffer.from([signatureWithHashType.length.toString()]),
-    signatureWithHashType, // ------------------s-
-    // Buffer.from([publicKey2.length]),
-    // Buffer.from(publicKey2),
-    Buffer.from([script.length.toString()]),
-    script,
-  ]);
-
-  const scriptLengthInput = scriptSig.length.toString(16).padStart(2, "0");
-
-  const rawTx = Buffer.concat([
-    Buffer.from(version, "hex"), // Versione della transazione
-    Buffer.from(tx_in_count, "hex"), // Numero di input
-    Buffer.from(hashTXReverse, "hex"), // Hash della transazione precedente (invertito)
-    Buffer.from(indexPrevOutput, "hex"), // Indice dell'output speso
-    Buffer.from(scriptLengthInput, "hex"), // Lunghezza del scriptSig
-    scriptSig, // ScriptSig per sbloccare i fondi
-    Buffer.from(sequence, "hex"), // Numero di sequenza
-    Buffer.from(tx_out_count, "hex"), // Numero di output
-    Buffer.from(valueOutput, "hex"), // Valore dell'output
-    Buffer.from(scriptLenghtOutput, "hex"), // Lunghezza del script di blocco (ScriptPubKey)
-    ScriptPubKey, // Script di blocco per l'output
-    Buffer.from(lookTime, "hex"), // Locktime
-    // Buffer.from(sigHashCode, "hex"), // Tipo di firma
-  ]);
-
-  console.log("la rawtx\n\n" + rawTx.toString("hex") + "\n\n");
-}
-
-main1();
